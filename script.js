2c7dd2a4b701f79550aacc991a8137bc7d7d8614
Sathwik P <sathwikputtala31@gmail.com>
2025-07-22 13:59:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let food=document.querySelector('.food')
+let food1=document.querySelector('#food1')
+let food2=document.querySelector('#food2')
+let food3=document.querySelector('#food3')
+let food4=document.querySelector('#food4')
+food1.addEventListener('click',function(){
+    food.style.backgroundImage="url('/images/food1.png')"
+})
+food2.addEventListener('click',function(){
+    food.style.backgroundImage="url('/images/food2.png')"
+})
+food3.addEventListener('click',function(){
+    food.style.backgroundImage="url('/images/food3.png')"
+})
+food4.addEventListener('click',function(){
+    food.style.backgroundImage="url('/images/food4.png')"
+})
