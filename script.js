f30e430549d74fc4c278e60fa2758eeee79e9002
Sathwik P <sathwikputtala31@gmail.com>
2025-07-22 14:03:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let food2=document.querySelector('#food2')
 let food3=document.querySelector('#food3')
 let food4=document.querySelector('#food4')
 food1.addEventListener('click',function(){
-    food.style.backgroundImage="url('/images/food1.png')"
+    food.style.backgroundImage="url('images/food1.png')"
 })
 food2.addEventListener('click',function(){
-    food.style.backgroundImage="url('/images/food2.png')"
+    food.style.backgroundImage="url('images/food2.png')"
 })
 food3.addEventListener('click',function(){
-    food.style.backgroundImage="url('/images/food3.png')"
+    food.style.backgroundImage="url('images/food3.png')"
 })
 food4.addEventListener('click',function(){
-    food.style.backgroundImage="url('/images/food4.png')"
+    food.style.backgroundImage="url('images/food4.png')"
 })
